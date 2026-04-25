e8ef83463874c0703372bd1c5e42737c9a028bae
palash khandelwal <palashkhandelwal2001@gmail.com>
2026-04-25 23:15:43 +0530

    direct commit to main branch

--- java-practise/Helloworld.java
+++ java-practise/Helloworld.java
@@ -1,6 +1,9 @@
 
 public class Helloworld {
     public static void main(String args[]){
+        int a = 10;
+
         System.out.println("HELLO WORLD");
+        System.out.println("value of a : "+a);
     }
 }
