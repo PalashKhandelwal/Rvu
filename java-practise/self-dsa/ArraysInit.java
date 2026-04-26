10ab6a80c01e05e98fbd66735118a3bda6ad3873
palash khandelwal <palashkhandelwal2001@gmail.com>
2026-04-26 13:19:26 +0530

    update

---
+++ java-practise/self-dsa/ArraysInit.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ArraysInit {
+    public static void main(String[] args) {
+        //limited initalization 
+        int arr[] = new int[5];
+
+        //dynamic memory array
+        ArrayList<String> arrayList = new ArrayList<>();
+        System.out.println("PRINTING THE LENGTH OF ARRAY 1 : "+arr.length);
+        System.out.println("PRINTING THE LENGTH OF ARRAY 2 : "+arrayList.size());
+    }
+}
